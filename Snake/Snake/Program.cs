52885287e6a091240fd951fadcc4a552a143b765
Vadzim <a.v.s.93@mail.ru>
2016-09-28 17:52:10 +0300

    линии

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,11 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '#');
-            p1.Draw();
+            //Point p1 = new Point(1, 3, '#');
+            //p1.Draw();
 
-            Point p2 = new Point(4, 5, '@');
-            p2.Draw();
+            //Point p2 = new Point(4, 5, '@');
+            //p2.Draw();
+
+            Horisontalr_line hl = new Horisontalr_line(3, 10, 5, '#');
+            hl.Drow();
+
+            Vertical_Line vl = new Vertical_Line(5, 10, 15, '#');
+            vl.Driw();
+
 
             Console.ReadKey();
 
