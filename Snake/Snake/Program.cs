25505ea3e5f7eaf90cf974e386d95991571cfd10
Vadzim <a.v.s.93@mail.ru>
2016-09-28 15:50:18 +0300

    Первый урок

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,6 +10,18 @@ namespace Snake
     {
         static void Main(string[] args)
         {
+            int X = Convert.ToInt32(Console.ReadLine());
+            int Y = Convert.ToInt32(Console.ReadLine());
+            char S = '#';
+            Draw(X, Y, S);
+            Console.ReadKey();
+        }
+
+         static void Draw (int x, int y, char s)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(s);
+
         }
     }
 }
