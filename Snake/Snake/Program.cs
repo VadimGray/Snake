976c6b800efaf438f42b1f0d15a913f9c40159fa
Vadzim <a.v.s.93@mail.ru>
2016-09-28 16:30:35 +0300

    Первый урок (reverted from commit 736e5def6062ec0e52dd60e90d52490c5cb528f0)

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,19 +10,17 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
-            p1.y = 3;
-            p1.s = '#';
-            p1.Draw();
-
-            Point p2 = new Point();
-            p2.x = 4;
-            p2.y = 5;
-            p2.s = '@';
-            p2.Draw();
-
+            int X = Convert.ToInt32(Console.ReadLine());
+            int Y = Convert.ToInt32(Console.ReadLine());
+            char S = '#';
+            Draw(X, Y, S);
             Console.ReadKey();
+        }
+
+         static void Draw (int x, int y, char s)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(s);
 
         }
     }
